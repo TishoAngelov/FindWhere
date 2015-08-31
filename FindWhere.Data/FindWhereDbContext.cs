74ffcad7dd5d582b3d31c25f158e12df550432ac
Tihomir Arahangelov <angelov.tisho@gmail.com>
2015-08-31 21:54:59 +0300

    Update

---
+++ FindWhere.Data/FindWhereDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace FindWhere.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Model;
+    using System.Data.Entity;
+
+    public class FindWhereDbContext : IdentityDbContext<User>
+    {
+        public FindWhereDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public static FindWhereDbContext Create()
+        {
+            return new FindWhereDbContext();
+        }
+
+        // TODO: Add all models
+        public virtual IDbSet<Country> Countries { get; set; }
+    }
+}
