61158d197657b8ceede2d33fcfc2b25169c410cc
Tihomir Arahangelov <angelov.tisho@gmail.com>
2015-09-01 19:19:06 +0300

    ...

---
+++ FindWhere/FindWhere.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace FindWhere.Common
+{
+    public static class GlobalConstants
+    {
+        public static string[] AllowedCountries = ["България"];
+
+        public static string[] AllowedCities = ["София"];
+    }
+}
---
+++ FindWhere/FindWhere.Model/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FindWhere.Model
+{
+    public class Location
+    {
+        public Location()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        public string PlaceId { get; set; }
+
+        public double Latitude { get; set; }
+    }
+}
