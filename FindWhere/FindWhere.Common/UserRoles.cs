5322b172db3fd59553b3d139df7afde6a1283492
Tihomir Arahangelov <angelov.tisho@gmail.com>
2015-09-02 22:08:01 +0300

    Update

---
+++ FindWhere/FindWhere.Common/UserRoles.cs
@@ -0,0 +1,11 @@
+﻿namespace FindWhere.Common
+{
+    public static class UserRoles
+    {
+        public static string Admin = "Admin";
+
+        public static string Moderator = "Moderator";
+
+        public static string User = "User";
+    }
+}
---
+++ FindWhere/FindWhere.Model/Neighbourhood.cs
@@ -0,0 +1,32 @@
+﻿namespace FindWhere.Model
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Neighbourhood
+    {
+        private ICollection<Location> locations;
+
+        public Neighbourhood()
+        {
+            this.locations = new HashSet<Location>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+        
+        public virtual int CityId { get; set; }
+
+        public virtual City City { get; set; }
+
+        public virtual ICollection<Location> Locations
+        {
+            get { return this.locations; }
+            set { this.locations = value; }
+        }
+    }
+}
